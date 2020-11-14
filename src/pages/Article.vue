b60eef0ac6eab915de9367902671450eface7269
TyomaMtl <scollioudfree.fr@gmail.com>
2020-11-14 11:43:37 +0100

    refactor(markdowns): rename folder

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -11,9 +11,9 @@ export default {
     beforeCreate() {
         let articles = datas['articles'].filter(article => article.id == this.$route.params.id)[0]
         if (articles != undefined) {
-            this.$options.components.markdown = () => import(`../articles/${this.$route.params.id}.md`)
+            this.$options.components.markdown = () => import(`../markdowns/${this.$route.params.id}.md`)
         } else {
-            this.$options.components.markdown = () => import(`../articles/default.md`)
+            this.$options.components.markdown = () => import(`../markdowns/default.md`)
         }
     }
 }
