937af455a911bf635f0de2fcfc020b90942e6758
TyomaMtl <scollioudfree.fr@gmail.com>
2020-11-14 11:38:46 +0100

    feat(article): show default markdown if article not found

--- src/pages/Article.vue
+++ src/pages/Article.vue
@@ -1,7 +1,5 @@
 <template>
     <div>
-        <p>{{ article.title }}</p>
-        <p>{{ id }}</p>
         <markdown/>
     </div>
 </template>
@@ -10,18 +8,13 @@
 import datas from '../datas'
 
 export default {
-    data() {
-        return {
-            id: this.$route.params.id,
+    beforeCreate() {
+        let articles = datas['articles'].filter(article => article.id == this.$route.params.id)[0]
+        if (articles != undefined) {
+            this.$options.components.markdown = () => import(`../articles/${this.$route.params.id}.md`)
+        } else {
+            this.$options.components.markdown = () => import(`../articles/default.md`)
         }
-    },
-    computed: {
-        article() {
-            return datas['articles'].filter(article => article.id == this.$route.params.id)[0]
-        }
-    },
-    beforeMount() {
-        this.$options.components.markdown = () => import(`../articles/${this.$route.params.id}.md`)
     }
 }
 </script>
