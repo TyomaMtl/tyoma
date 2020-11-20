2f17601ce4725ea4df5e0d019b9a3366ff310da1
TyomaMtl <scollioudfree.fr@gmail.com>
2020-11-20 17:59:08 +0100

    feat(portfolio): add link to socials and projets

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -1,9 +1,17 @@
 <template>
-    <div class="portfolio">
-        <div class="logo">Tyoma</div>
-        <div class="jumbo">
-            <h1>My name is not Tyoma</h1>
+    <div>
+        <div class="portfolio">
+            <div class="logo">Tyoma</div>
+            <div class="socials">
+                <ul>
+                    <li><a target="_blank" href="https://github.com/TyomaMtl"><i class="icon-github"></i></a></li>
+                </ul>
+            </div>
+            <div class="jumbo">
+                <h1>My name is not Tyoma</h1>
+            </div>
+            <router-link class="link blog" :to=" { name: 'blog' }">Blog</router-link>
+            <router-link class="link projects" :to=" { name: 'blog' }">Projects</router-link>
         </div>
-        <router-link class="blog-link" :to=" { name: 'blog' }">Blog</router-link>
     </div>
 </template>
