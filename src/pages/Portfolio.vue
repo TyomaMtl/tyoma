8fb8a1c53a45f074796eaa972591aba10d1577b4
TyomaMtl <scollioudfree.fr@gmail.com>
2020-11-24 19:21:05 +0100

    feat(portfolio): create about section

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -13,5 +13,15 @@
             <router-link class="link blog" :to=" { name: 'blog' }">Blog</router-link>
             <router-link class="link projects" :to=" { name: 'blog' }">Projects</router-link>
         </div>
+        <div class="about">
+            <div>
+                <a href="mailto:hello@tyoma.co">hello@tyoma.co</a>
+            </div>
+            <div>
+                <h3>About</h3>
+                <p>My name is not Tyoma</p> 
+                <p>I'm a fullstack developper from France. I create tailored website and help you turning your ideas into real digital project</p>
+            </div>
+        </div>
     </div>
 </template>
