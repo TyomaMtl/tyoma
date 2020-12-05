480ec083c2f48f5a7c2844cc7a203486ac7ce930
TyomaMtl <scollioudfree.fr@gmail.com>
2020-12-05 13:06:17 +0100

    feat(timeline): scroll to project year

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -6,10 +6,10 @@
         <section class="projects">
             <ul class="timeline">
                 <p>Projects</p>
-                <li v-for="year in years" :key="year">{{ year }}</li>
+                <li :class="[year == currentYear ? 'active' : '']" v-for="year in years" :key="year" v-on:click="scrollToYear(year)">{{ year }}</li>
             </ul>
-            <div class="project-wrapper">
-                <div v-for="project in projects" :key="project.id" class="project">
+            <div class="project-wrapper" ref="projects" v-on:scroll="handleScroll()">
+                <div v-for="project in projects" :key="project.id" class="project" :ref="project.year" :data-year="project.year">
                     <img :src="project.cover">
                     <a class="content" href="#">
                         <h4>{{ project.title }}</h4>
@@ -27,11 +27,37 @@ import datas from '../datas'
 export default {
     data () {
         return {
+            currentYear: new Date().getFullYear(),
             projects: datas.projects
         }
     },
+    methods: {
+        scrollToYear: function (year) {
+            var project = this.$refs[year]
+            this.currentYear = year
+            if (project) {
+                project[0].scrollIntoView({ behavior: 'smooth' })
+            }
+        },
+        handleScroll: function () {
+            const projects = this.$refs.projects
+            const wrapperRect = projects.getBoundingClientRect()
+            var visibleElements = []
+
+            // find visible projects
+            projects.children.forEach(project => {
+                var projectRect = project.getBoundingClientRect()
+                if (projectRect.left >= wrapperRect.left && projectRect.right - 10 <= wrapperRect.right) {
+                    visibleElements.push(project)
+                }
+            })
+
+            // set current year of the last visible project
+            this.currentYear = visibleElements[visibleElements.length - 1].dataset.year
+        }
+    },
     computed: {
-        years() {
+        years: function() {
             var now = new Date().getFullYear()
             var start = 2017
             var years = []
